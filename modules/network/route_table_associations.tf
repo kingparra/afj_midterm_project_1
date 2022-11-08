adbe7ccb6dfcf90b3b6a8c6d2bf925a0fc33b8c7
Chris King-Parra <chris@kingparra.work>
2022-11-07 22:24:14 -0500

    Replace ids with references in rtas

--- modules/network/route_table_associations.tf
+++ modules/network/route_table_associations.tf
@@ -8,24 +8,18 @@ resource "aws_route_table_association" "midterm-rtb-public-rtbassoc-041bd566420f
 
 # Associate route table
 resource "aws_route_table_association" "midterm-rtb-public-rtbassoc-09d3e79eb1af2a84a" {
-    # midterm-rtb-public
-    route_table_id = "rtb-05017698ed8b0d36f"
-    # midterm-subnet-public1-us-east-1a
-    subnet_id = "subnet-0c9e06017eba2d0fb"
+    route_table_id = aws_route_table.midterm-rtb-public.id
+    subnet_id = aws_subnet.subnet-0c9e06017eba2d0fb-midterm-subnet-public1-us-east-1a.id
 }
 
 # Associate route table
 resource "aws_route_table_association" "midterm-rtb-prib8ef2aaa28ec96" {
-    # midterm-rtb-private1-us-east-1a
-    route_table_id = "rtb-0c878fd2064022d80"
-    # midterm-subnet-private1-us-east-1a
-    subnet_id = "subnet-018236422151345f7"
+    route_table_id = aws_route_table.midterm-rtb-private1-us-east-1a.id
+    subnet_id = aws_subnet.subnet-018236422151345f7-midterm-subnet-private1-us-east-1a.id
 }
 
 # Associate route table
 resource "aws_route_table_association" "midterm-rtb-pri4ccdb4b4bc43ce" {
-    # rtb-08840b3dbc414d5b1
-    route_table_id = "rtb-08840b3dbc414d5b1"
-    # midterm-subnet-private2-us-east-1b
-    subnet_id = "subnet-0ba3d357be0deb0de"
+    route_table_id = aws_route_table.midterm-rtb-private2-us-east-1b.id
+    subnet_id = aws_subnet.subnet-0ba3d357be0deb0de-midterm-subnet-private2-us-east-1b.id
 }
