190bc85d049ccaae05c9ae5ef05aa8113084922f
Chris King-Parra <chris@kingparra.work>
2022-11-13 20:31:46 -0500

    Rename subnets

--- modules/network/outputs.tf
+++ modules/network/outputs.tf
@@ -3,17 +3,17 @@ output "vpc_id" {
 }
 
 output "subnet_public1_id" {
-  value = aws_subnet.subnet-0c9e06017eba2d0fb-midterm-subnet-public1-us-east-1a.id
+  value = aws_subnet.subnet-public-1.id
 }
 
 output "subnet_public2_id" {
-  value = aws_subnet.subnet-03b9c803fafb82de5-midterm-subnet-public2-us-east-1b.id
+  value = aws_subnet.midterm-subnet-public2.id
 }
 
 output "subnet_private1_id" {
-  value = aws_subnet.subnet-018236422151345f7-midterm-subnet-private1-us-east-1a.id
+  value = aws_subnet.midterm-subnet-private1.id
 }
 
 output "subnet_private2_id" {
-  value = aws_subnet.subnet-0ba3d357be0deb0de-midterm-subnet-private2-us-east-1b.id
+  value = aws_subnet.midterm-subnet-private2.id
 }
--- modules/network/route_table_associations.tf
+++ modules/network/route_table_associations.tf
@@ -1,23 +1,23 @@
 # Associate route table
 resource "aws_route_table_association" "midterm-rtb-public-rtbassoc-041bd566420f8e9f7" {
     route_table_id = aws_route_table.midterm-rtb-public.id
-    subnet_id = aws_subnet.subnet-03b9c803fafb82de5-midterm-subnet-public2-us-east-1b.id
+    subnet_id = aws_subnet.midterm-subnet-public2.id
 }
 
 # Associate route table
 resource "aws_route_table_association" "midterm-rtb-public-rtbassoc-09d3e79eb1af2a84a" {
     route_table_id = aws_route_table.midterm-rtb-public.id
-    subnet_id = aws_subnet.subnet-0c9e06017eba2d0fb-midterm-subnet-public1-us-east-1a.id
+    subnet_id = aws_subnet.subnet-public-1.id
 }
 
 # Associate route table
 resource "aws_route_table_association" "midterm-rtb-prib8ef2aaa28ec96" {
     route_table_id = aws_route_table.midterm-rtb-private1-us-east-1a.id
-    subnet_id = aws_subnet.subnet-018236422151345f7-midterm-subnet-private1-us-east-1a.id
+    subnet_id = aws_subnet.midterm-subnet-private1.id
 }
 
 # Associate route table
 resource "aws_route_table_association" "midterm-rtb-pri4ccdb4b4bc43ce" {
     route_table_id = aws_route_table.midterm-rtb-private2-us-east-1b.id
-    subnet_id = aws_subnet.subnet-0ba3d357be0deb0de-midterm-subnet-private2-us-east-1b.id
+    subnet_id = aws_subnet.midterm-subnet-private2.id
 }
