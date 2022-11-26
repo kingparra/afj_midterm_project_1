1b1f1ca31e9882a53dc178e3576da8a98fc019e3
Chris King-Parra <chris@kingparra.work>
2022-11-26 09:31:30 -0500

    Add webserver ip address to outputs

--- modules/ec2_instance/output.tf
+++ modules/ec2_instance/output.tf
@@ -2,3 +2,7 @@ output "private_key" {
   sensitive = true
   value = tls_private_key.key_pair_precision.private_key_openssh
 }
+
+output "webserver_ip" {
+  value = aws_instance.web.public_ip
+}
--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,7 @@ output "private_key" {
   sensitive = true
   value = module.ec2_instance.private_key
 }
+
+output "webserver_ip" {
+  value = module.ec2_instance.webserver_ip
+}
